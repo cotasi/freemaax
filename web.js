b8d5ca4f72f9a93cf06f4f630106a5ac11d11770
cotasi <cotasi135@gmail.com>
2023-12-15 15:57:03 +0900

    sipal

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT || 8001;
 
 app.get('/',function(req, res){
-    res.sendFile( path.join(__dirname, './front/public/index.html'))
+    res.sendFile( path.join(__dirname, 'front/public/index.html'))
 })
 
 //라우터없다면

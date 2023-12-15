36b0edfa6b926db53570bb2fd3597ceaacd6980f
cotasi <cotasi135@gmail.com>
2023-12-15 16:33:12 +0900

    routernone

--- web.js
+++ web.js
@@ -9,11 +9,6 @@ app.get('/',function(req, res){
     res.sendFile( path.join(__dirname, 'front/public/index.html'))
 })
 
-//라우터없다면
- app.use((req, res) =>{
-    res.status(404).sendFile( path.join(__dirname, 'public/nopage.html'))
-})
-
 app.listen(PORT, () => {
     console.log( `${PORT} 노드서버구동정상` )
  })

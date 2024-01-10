8ed2c36947986e077585866ff34489e93563cf1d
cotasi <cotasi135@gmail.com>
2024-01-10 23:03:39 +0900

    dd

--- front/src/comp/Contents1.js
+++ front/src/comp/Contents1.js
@@ -18,6 +18,59 @@ import { FreeMode, Pagination, Navigation, Controller } from 'swiper/modules';
 
 
 const Contents1 = () => {
+    function csvToJSON(csv_string){
+
+        // 1. 문자열을 줄바꿈으로 구분 => 배열에 저장
+        const rows = csv_string.split("\r\n");
+        
+        // 줄바꿈을 \n으로만 구분해야하는 경우, 아래 코드 사용
+        // const rows = csv_string.split("\n");
+      
+    
+        // 2. 빈 배열 생성: CSV의 각 행을 담을 JSON 객체임
+        const jsonArray = [];
+    
+      
+        // 3. 제목 행 추출 후, 콤마로 구분 => 배열에 저장
+        const header = rows[0].split(",");
+    
+      
+        // 4. 내용 행 전체를 객체로 만들어, jsonArray에 담기
+        for(let i = 1; i < rows.length; i++){
+      
+            // 빈 객체 생성: 각 내용 행을 객체로 만들어 담아둘 객체임
+            let obj = {};
+    
+            // 각 내용 행을 콤마로 구분
+            let row = rows[i].split(",");
+      
+            // 각 내용행을 {제목1:내용1, 제목2:내용2, ...} 형태의 객체로 생성
+            for(let j=0; j < header.length; j++){
+                obj[header[j]] = row[j];
+            }
+      
+            // 각 내용 행의 객체를 jsonArray배열에 담기
+            jsonArray.push(obj);
+      
+        }
+        
+        // 5. 완성된 JSON 객체 배열 반환
+        return jsonArray;
+    
+        // 문자열 형태의 JSON으로 반환할 경우, 아래 코드 사용
+        // return JSON.stringify(jsonArray);
+    }
+
+    const csv_string = `업체명,전화번호,차고지,대표자,노선번호,운행대수,예비운행대수,소요시간(분),운행거리(km),배차간격(분),첫차,막차,운행구간
+    영도여객㈜,051-412-3440,부산광역시 영도구 해양로 128(청학동),이막례,영도구1,3,1,36,8.8,18,6:20,21:45,홈플러스-대교사거리-강남병원-흰여울문화마을-유림아파트
+    영도여객㈜,051-412-3440,부산광역시 영도구 해양로 128(청학동),이막례,영도구2,3,0,36,7.8,18,6:15,21:35,영선동반도보라아파트-대평동-(구)전차종점-선용품유통센터-영선동반도보라아파트-남항초등학교-흰여울문화마을-체육공원
+    ㈜대일버스,051-403-7782,부산광역시 영도구 조내기로 74(청학동),최재영,영도구5,10,1,80,45,19,6:00,22:25,동삼그린힐-영도구청-고신대-중복도로-이송도-남포역-연안부두
+    ㈜대일버스,051-403-7782,부산광역시 영도구 조내기로 74(청학동),최재영,영도구7,1,0,18,5.3,30,6:00,22:00,청학동시장-부산은행-미주아파트-광명고-롯데낙천대-82번종점-청학동시장`;
+
+    const busan_json = csvToJSON(csv_string);
+
+    console.log(busan_json);
+
 
     const [firstswiper,setfirstswiper] = useState(null);
     const [secondswiper,setsecondswiper] = useState(null);

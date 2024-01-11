0ad8461ea718f8a0889bed35c5bc3ea9c7a5058f
cotasi <cotasi135@gmail.com>
2024-01-11 12:58:53 +0900

    form

--- front/src/comp/Query.js
+++ front/src/comp/Query.js
@@ -4,12 +4,12 @@ import Q from '../scss/query.module.scss';
 import QNavi from './QNavi';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import { useForm } from 'react-hook-form';
-
-
+import { useForm, SubmitHandler } from 'react-hook-form';
 
 const Query = () => {
     const {register, handleSubmit,watch,formState: {errors}} = useForm();
+
+    const onSubmit = (data) => console.log(data);
 
     return (
     <>
@@ -18,15 +18,15 @@ const Query = () => {
         <h2><span>상담 양식을</span> 확인해주세요</h2>
         <h3>고객님의 의견을 적극 반영하겠습니다.</h3>
         <div className={`${Q.querycon}`}>
-            <Form onSubmit={handleSubmit((data)=> alert(JSON.stringify(data)))}>
+            <Form onSubmit = {handleSubmit(onSubmit)}>
                 <h2>상담정보</h2>
                 <Form.Group>
                     <Form.Label>이름</Form.Label>
-                    <Form.Control required type="text" placeholder="이름을 입력해주세요."></Form.Control>
+                    <Form.Control defaultValue="test" {...register("example")}required type="text" placeholder="이름을 입력해주세요."></Form.Control>
                 </Form.Group>
                 <Form.Group>
                     <Form.Label>이메일</Form.Label>
-                    <Form.Control required type="text" placeholder="이름을 입력해주세요."></Form.Control>
+                    <Form.Control {...register("example required", { required: true })} required type="text" placeholder="이름을 입력해주세요."></Form.Control>
                 </Form.Group>
                 <Form.Group>
                     <Button type="submit">확인</Button>

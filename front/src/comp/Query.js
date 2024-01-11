6c81c0d016b1d115b09a778e28e7d6226b49ecc2
cotasi <cotasi135@gmail.com>
2024-01-11 14:00:29 +0900

    responsive

--- front/src/comp/Query.js
+++ front/src/comp/Query.js
@@ -27,7 +27,7 @@ const Query = () => {
                 </Form.Group>
                 <Form.Group>
                     <Form.Label>이메일</Form.Label>
-                    <Form.Control {...register("example required", { required: true })} name="emails" required type="text" placeholder="이름을 입력해주세요."></Form.Control>
+                    <Form.Control {...register("example required", { required: true })} required type="text" placeholder="이름을 입력해주세요."></Form.Control>
                     {errors.exampleRequired && <span>반드시 입력해주세요.</span>}
                 </Form.Group>
                 <Form.Group>

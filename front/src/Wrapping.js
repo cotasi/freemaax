b0fa06235ccda8ef452fb7b3fb5de0af8b68a25f
cotasi <cotasi135@gmail.com>
2024-01-11 11:01:20 +0900

    modals

--- front/src/Wrapping.js
+++ front/src/Wrapping.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import HeaderAll from './comp/HeaderAll';
 import Main from './comp/Main';
 import CI from './comp/CI';
 import Query from './comp/Query';
 import { Route, Routes } from 'react-router-dom';
+import Modals from './comp/Modal';
 
 const Wrapping = () => {
+    useEffect(()=>{
+    },[]);
+
     return (
         <div>
             <HeaderAll />
+            <Modals></Modals>
             <Routes>
                 <Route path="/" element={<Main></Main>}></Route>
                 <Route path="/com/ci" element={<CI></CI>}></Route>
---
+++ front/src/comp/Modal.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Md from '../scss/modal.module.scss';
+
+const Modal = () => {
+    return (
+        <div className={`${Md.modals}`}>
+            <div className={`${Md.imagewraps}`}>
+                <img src="/img/openbanner" alt="openbanner" />
+            </div>
+            <div className={`${Md.buttonwraps} d-flex`}>
+                <button className="col">오늘 하루 더 이상 안보기</button>
+                <button className="col">닫기</button>
+            </div>
+        </div>
+    );
+};
+
+export default Modal;

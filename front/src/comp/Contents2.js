a26750570e7d5904075864fe79ada8bbb8106ac2
cotasi <cotasi135@gmail.com>
2024-01-11 10:39:46 +0900

    c2remove

--- front/src/comp/Contents2.js
+++ front/src/comp/Contents2.js
@@ -10,32 +10,7 @@ import { FreeMode, Pagination } from 'swiper/modules';
 const Contents2 = () => {
     return (
         <div className={`${C2.c2main}`}>
-            <h2>프리맥스 예약 시스템</h2>
-            <div className={`${C2.c2swiperwr}`}>
-                <Swiper slidesPerView={3} spaceBetween={30} modules={[FreeMode]}>
-                    {
-                        Data[0].swiper[2].swiper3.map((e,i)=>{
-                            return(
-                                <SwiperSlide className={`${C2.swiperslide}`}>
-                                    <div className={`${C2.swiperimgwrap}`}><img src={e.swiper3img} alt={i} /></div>
-                                    <div className={`${C2.swipertextpart}`}>
-                                        <div>{e.swiper3text1}({e.swiper3arrive})</div>
-                                        <div>{e.swiper3ment.split('/').map((ee,ii)=>{
-                                            return(
-                                                <div>{ee}</div>
-                                            )
-                                        })}</div>
-                                        <div>
-                                            <div>성인 ${e.swiperprice.split('/')[0]}</div>
-                                            <div>청소년 ${e.swiperprice.split('/')[1]}</div>
-                                        </div>
-                                    </div>
-                                </SwiperSlide>
-                            )
-                        })
-                    }
-                </Swiper>
-            </div>
+            
         </div>
     );
 };

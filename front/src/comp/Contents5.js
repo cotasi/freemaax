a7283c6f74542aea548ba83c1bb99d5ef8d80fc0
cotasi <cotasi135@gmail.com>
2024-01-12 19:03:12 +0900

    dd

--- front/src/comp/Contents5.js
+++ front/src/comp/Contents5.js
@@ -2,7 +2,23 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Noticepresswrap = styled.div`
-    width: 100%;
+    width: 60%;
+    margin: 0 auto;
+    & h2 {
+        font-size: 1.5em;
+        position: relative;
+        &:after {
+            content: '';
+            position: absolute;
+            display: block;
+            left: 0;
+            right: 0;
+            bottom: 23%;
+            height: 2px;
+            background-color: black;
+        }
+    }
+
 `;
 
 const Contents5 = () => {
